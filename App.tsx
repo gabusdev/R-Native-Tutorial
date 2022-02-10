d0251af773d2bcf096d00d7331bc0281cdc21efa
gabusdev <gabusdev@outlook.com>
2022-02-10 12:54:47 -0500

    Using useState

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("gabus");
+  const [person, setPerson] = useState({ name: "Mario", age: 40 });
+
+  const clickHandle = () => {
+    setName("Gabo");
+    setPerson({ name: "Luigi", age: 45 });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Lorem ipsum dolor sit amet...</Text>
-        <Text>Lorem ipsum dolor sit amet...</Text>
-        <Text>Lorem ipsum dolor sit amet...</Text>
-        <Text>Lorem ipsum dolor sit amet...</Text>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Change Name' onPress={clickHandle} />
       </View>
       <StatusBar style='auto' />
     </View>
@@ -35,5 +42,8 @@ const styles = StyleSheet.create({
   body: {
     backgroundColor: "yellow",
     padding: 20,
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
