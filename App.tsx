64b705ac5231ccbfd91f18b0f73d2ead43d67abb
gabusdev <gabusdev@outlook.com>
2022-02-10 14:14:32 -0500

    list and scroll view

--- App.tsx
+++ App.tsx
@@ -1,33 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, StyleSheet, Text, ScrollView, View } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("user");
-  const [age, setAge] = useState(30);
-
-  const clickHandle = () => {
-    setName("");
-  };
+  const [people, setPeople] = useState([
+    { name: "Mario", key: 1 },
+    { name: "Luigi", key: 2 },
+    { name: "Bowser", key: 3 },
+    { name: "Peach", key: 4 },
+    { name: "DK", key: 5 },
+    { name: "Yoshi", key: 6 },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>Enter Name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder='e.g. John Doe'
-        onChangeText={(val) => setName(val)}
-      />
-      <Text>Enter Age:</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType='numeric'
-        placeholder='e.g. 30'
-        onChangeText={(val) => setAge(Number(val))}
-      />
-      <Text>Name is: {name}</Text>
-      <Text>Age is: {age}</Text>
+      <ScrollView>
+        {people.map((item) => (
+          <View>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
       <StatusBar style='auto' />
     </View>
   );
@@ -37,14 +30,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item: {
+    padding: 30,
+    marginTop: 24,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
