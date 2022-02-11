24a53f30d4c32e87e5203ab72a3bc2cf81f3a69c
gabusdev <gabusdev@outlook.com>
2022-02-11 16:21:42 -0500

    added icons

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -6,6 +6,7 @@ import {
   FlatList,
   View,
 } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 interface TodoItemProps {
   item: { text: string; key: any };
@@ -15,19 +16,26 @@ interface TodoItemProps {
 const TodoItem: FunctionComponent<TodoItemProps> = ({ item, handler }) => {
   return (
     <TouchableOpacity onPress={() => handler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name='delete' size={18} color='#333' />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   item: {
+    flexDirection: "row",
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+  },
+  itemText: {
+    marginLeft: 10,
   },
 });
 
